9af18bf16909475f95d2089815729743dcb9be6c
cloud.liu <cloud.liu@imaginato.com>
2014-02-18 18:31:49 +0800

    add the test image

--- protected/views/swipeJs/index.php
+++ protected/views/swipeJs/index.php
@@ -9,7 +9,7 @@
 <div>
     一个在移动端下的触摸滑动菜单， 支持pc端的鼠标拖拽
 </div>
-<div id="menu" class='swipe'>
+<div id="slideMenu" class='swipe'>
     <ul >
         <li>cat</li>
         <li>cats</li>
@@ -48,4 +48,63 @@
         });
     });
 
+</script>
+
+
+<?php
+
+
+$this->widget('zii.widgets.grid.CGridView', array(
+    'dataProvider'=> $model,
+//    'filter'=>$model,
+//    'columns'=>array(
+//
+//        array(
+//            'name'=>'work_date',
+//            'type'=>'datetime',
+//            'filter'=>false,
+//        ),
+//        array(
+//            'class'=>'CButtonColumn',
+//        ),
+//    ),
+));
+
+$this->widget('toolWidget');
+//$this->beginWidget('path.to.toolWidget');
+//$this->endWidget();
+?>
+
+<style>
+    #img-window{ width:310px; height:auto; overflow: hidden; position:relative; height:600px}
+    .img-list{ margin:0; padding:0; width:960px; position:absolute}
+    .img-list li{ float:left }
+</style>
+<div class="img-p">
+    <p>第一个</p>
+    <p>第二个</p>
+    <p>第三个</p>
+</div>
+<div id="img-window">
+    <ul class="img-list">
+        <li><img src="/assets/img/swipe/bleach0.jpg"></li>
+        <li><img src="/assets/img/swipe/bleach1.jpg"></li>
+        <li><img src="/assets/img/swipe/bleach2.jpg"></li>
+    </ul>
+</div>
+
+<script>
+
+    (function(){
+
+        $('.img-p p').mouseover(function(){
+            var index = $(this).index();
+            var width = $('#img-window').width();
+
+            var leftPos = ( - index * width ) + 'px';
+            $('.img-list').animate({
+                left : leftPos
+            },'slow');
+        });
+    })();
 </script>
