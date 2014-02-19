18e9a6d7e320ee54c0c09d72ccbb80431a551d35
cloud <792724991@qq.com>
2014-02-19 23:41:43 +0800

    add fixy theme

---
+++ blog/wp-content/themes/fixy/footer.php
@@ -0,0 +1,30 @@
+<?php
+/* The Template for displaying footer area.
+ *
+ * @package WordPress
+ * @subpackage fixy
+ * @since Fixy 0.1.0
+ */
+?>
+				</div><!--.content-->
+			</div><!--.main-->
+
+		
+		<div id="footer" class="footer">
+			
+			<?php get_sidebar( 'footer' ); ?>
+			
+			<div class="site-credit">
+				<p><?php echo get_bloginfo('name').__(' Copyright (C)','fixy').get_the_date('Y').__(' .All Rights Reserved.','fixy'); ?><br/>
+				<?php echo __('Powered by WordPress. Theme by ','fixy').'<a href="http://www.itstar.ir">ITstar</a>' ?></p>
+			</div><!--.site-credit-->
+			
+		</div><!--.footer-->
+		<span class="external-ok" endis="<?php echo esc_attr(get_theme_mod('fixy_external_links_show')); ?>"></span>
+		<span class="lazy-ok" endis="<?php echo esc_attr(get_theme_mod('fixy_lazy_content')); ?>"></span>
+		 <?php get_template_part('lib/ajax','forms'); ?>
+		
+		<?php wp_footer(); ?>
+</div><!--.page-->
+  	</body><!--body-->
+</html><!--html>
