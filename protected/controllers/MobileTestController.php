ccb9d573eee2a7117ba16b176264c93ed47f726a
cloud <792724991@qq.com>
2014-03-23 22:51:14 +0800

    the lost controller

---
+++ protected/controllers/MobileTestController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 14-2-8
+ * Time: 下午4:07
+ * To change this template use File | Settings | File Templates.
+ */
+class MobileTestController extends Controller{
+
+    public function actionIndex(){
+
+        $this->render('index',array(
+
+        ));
+    }
+}
