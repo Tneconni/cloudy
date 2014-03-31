eedde76b88dfbb15080beaba6c9b6722c51c0c53
cloud <792724991@qq.com>
2014-03-31 23:24:01 +0800

    the fish project

--- protected/views/form/index.php
+++ protected/views/form/index.php
@@ -1,7 +1,24 @@
-<div class="row" ng-app>
+<div class="row" ng-app="fisher"  ng-controller="fish">
 
-    <h3>Angular</h3>
-    <div> guaiguai </div>
+    <h3>FISHER</h3>
+
+    <div ng-controller="fishType">
+        <h3>Fish Type</h3>
+        <div  class="fish-type">
+            <div class="fish-type-single" ng-repeat=" type in fishType ">
+                <span class="type-id" style="display:none">{{type.id}}</span>
+                <span class="type-name">{{type.name}}</span>
+            </div>
+        </div>
+        <div>
+            <input type="text" ng-keyup="" value="" />
+            <div>add</div>
+        </div>
+
+        <div>
+            你现在得到的食物是 {{ allFood }}
+        </div>
+    </div>
 
     <div ng-controller="catchFood">
         <div>
@@ -34,6 +51,85 @@
 
 <script>
 
+    //todo try code a service
+    var app = angular.module("fisher", []);
+    app.factory('searchFisher', function(){
+
+        var fishTypeDom = angular.element('.fish-type');
+        var fishTypes = [];
+
+        // not get the fish type , because the type has not load at this time
+        angular.forEach( fishTypeDom, function( dom, index ){
+
+            console.log('index :' + index );
+            var typeDom = angular.element( dom );
+            console.log( this );
+            console.log( typeDom );
+            console.log( typeDom.find('span') );
+            console.log( typeDom.find('span').eq(0) );
+            var singleType = {
+                id : typeDom.find('span').eq(0).html(),
+                name : typeDom.find('span').eq(1).html()
+            };
+            fishTypes.push = singleType;
+
+        } );
+        var ret = {
+            'food' : 'asd',
+            'fisher' : 'esc',
+            getFishType : function( ){
+                return {
+                    count : 3,
+                    type : fishTypes
+                };
+            }
+        };
+
+        return ret;
+
+    });
+
+
+    function fishType( $scope ){
+        $scope.fishType = [
+            {
+                id : 1,
+                name : 'liyu'
+            },
+            {
+                id : 3,
+                name : 'jinyu'
+            },
+            {
+                id : 2,
+                name : 'caoyu'
+            }
+        ];
+
+
+    }
+
+    app.controller("fish", ['$scope', 'searchFisher', function($scope, searchFisher ){
+
+        var type = searchFisher.getFishType();
+        console.log( type );
+        $scope.$on("changeFoodA", function( event, msg ){
+            console.log( 'changeFoodA', msg);
+
+            $scope.$broadcast("sendFoodA", msg);
+        });
+
+    }]);
+
+//    function fish( $scope, $http){
+//
+//        $scope.$on("changeFoodA", function( event, msg ){
+//            console.log( 'changeFoodA', msg);
+//
+//            $scope.$broadcast("sendFoodA", msg);
+//        });
+//    }
+
     function catchFood( $scope ){
         $scope.foodA = 'carrert';
         $scope.foodB = 'bread';
@@ -44,7 +140,12 @@
 
             $scope.allFood = $scope.foodA + $scope.foodB + $scope.foodC;
 
+            //todo emit a change to parent controller
+            $scope.$emit( "changeFoodA", $scope.foodA );
+
         };
+
+
 
     }
 
@@ -62,6 +163,11 @@
             }
 
         };
+
+        $scope.$on( "sendFoodA", function( event, msg ){
+            console.log( event );
+            console.log("get the newest foodA", msg);
+        });
     }
 
 </script>
