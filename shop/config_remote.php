51a84ad948dd62b483945b26032944aa634cc655
cloud <792724991@qq.com>
2014-05-13 23:55:20 +0800

    the remote config

---
+++ shop/config_remote.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tneconni.gotoip55.com/shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://tneconni.gotoip55.com/shop/');
+
+// DIR
+define('DIR_APPLICATION', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/catalog/');
+define('DIR_SYSTEM', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/system/');
+define('DIR_DATABASE', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/system/database/');
+define('DIR_LANGUAGE', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/catalog/language/');
+define('DIR_TEMPLATE', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/catalog/view/theme/');
+define('DIR_CONFIG', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/system/config/');
+define('DIR_IMAGE', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/image/');
+define('DIR_CACHE', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/system/cache/');
+define('DIR_DOWNLOAD', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/download/');
+define('DIR_LOGS', '/home/tneconni6tqnae1cgopnandi/wwwroot/shop/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'tneconni');
+define('DB_PASSWORD', 'phxehn6v');
+define('DB_DATABASE', 'tneconni');
+define('DB_PREFIX', 'oc_');
+?>
