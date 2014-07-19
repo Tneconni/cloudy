b9fd11c1a12fd64944cd6ade22c83afa07ffcaf2
cloud <792724991@qq.com>
2014-07-19 09:18:03 +0800

    search_bar

--- shop/catalog/controller/common/search_bar.php
+++ shop/catalog/controller/common/search_bar.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 14-7-19
- * Time: 上午8:30
- */ 
+class ControllerCommonSearchBar extends Controller {
+    protected function index() {
+
+        $this->data['home'] = $this->url->link('common/home');
+
+        if (isset($this->request->get['search'])) {
+            $this->data['search'] = $this->request->get['search'];
+        } else {
+            $this->data['search'] = '';
+        }
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/search_bar.tpl')) {
+            $this->template = $this->config->get('config_template') . '/template/common/search_bar.tpl';
+        }else {
+            $this->template = 'default/template/common/search_bar.tpl';
+        }
+
+        $this->render();
+    }
+}
+?>
