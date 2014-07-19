8dac4df1b737018b72ceaf0840ba97463d006f61
cloud <792724991@qq.com>
2014-07-19 09:17:24 +0800

    navigation

--- shop/catalog/controller/common/navigation.php
+++ shop/catalog/controller/common/navigation.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 14-7-19
- * Time: 上午8:30
- */ 
+class ControllerCommonNavigation extends Controller {
+    protected function index() {
+
+
+        $this->children = array(
+
+            'common/nav_left'
+
+        );
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/navigation.tpl')) {
+            $this->template = $this->config->get('config_template') . '/template/common/navigation.tpl';
+        }else {
+            $this->template = 'default/template/common/navigation.tpl';
+        }
+
+        $this->render();
+    }
+}
+?>
