e6b97369e70e8621262216f3b93c04b8b49172e6
792724991@qq.com <innocent2>
2014-10-10 13:01:52 +0800

    for wechat check

--- library/wechat/Api.php
+++ library/wechat/Api.php
@@ -34,9 +34,9 @@ class Api {
     public function responseMsg()
     {
         echo 'responseMsg';
+        var_dump($GLOBALS["HTTP_RAW_POST_DATA"]);
         //get post data, May be due to the different environments
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-        echo 'faafafaf';
         //extract post data
         if (!empty($postStr)){
 
