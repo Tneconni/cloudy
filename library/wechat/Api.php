ec36e4e098cd91b140c39f6516e6c3056f73716e
cloud <792724991@qq.com>
2014-10-10 23:12:12 +0800

    the response

--- library/wechat/Api.php
+++ library/wechat/Api.php
@@ -53,12 +53,22 @@ class Api {
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
 							</xml>";
+
+
             if(!empty( $keyword ))
             {
-                $msgType = "text";
-                $contentStr = "欢迎光临漫客小屋!";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
+//                $msgType = "text";
+//                $contentStr = "欢迎光临漫客小屋!";
+//                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+
+                $param = array(
+                    'fromUsername'=> $fromUsername,
+                    'toUsername'=>$toUsername,
+                    'time'=>$time,
+                    'msgType'=>'news'
+                );
+                $response = $this->getResponseTmp( $param );
+                echo $response;
             }else{
                 echo "Input something...";
             }
@@ -69,7 +79,33 @@ class Api {
         }
     }
 
+    public function getResponseTmp( $data ){
 
+        $xmlStart = "<xml>
+<ToUserName><![CDATA[" . $data['fromUsername'] . "]]></ToUserName>
+<FromUserName><![CDATA[" . $data['toUsername'] . "]]></FromUserName>
+<CreateTime>" . $data['time'] . "</CreateTime>
+<MsgType><![CDATA[" . $data['msgType'] . "]]></MsgType>
+<ArticleCount>2</ArticleCount>
+<Articles>";
+
+        $xmlEnd = "</Articles></xml> ";
+
+            $itemTmp = "<item>
+<Title><![CDATA[漫客小屋]]></Title>
+<Description><![CDATA[漫迷落脚的小屋]]></Description>
+<PicUrl><![CDATA[https://mp.weixin.qq.com/cgi-bin/getimgdata?token=496425256&msgid=&mode=large&source=file&fileId=200464405&ow=-1]]></PicUrl>
+<Url><![CDATA[http://121/42/42/110/cloudy/app/poker]]></Url>
+</item><item>
+<Title><![CDATA[漫客小屋]]></Title>
+<Description><![CDATA[选出你心目中的英雄]]></Description>
+<PicUrl><![CDATA[https://mp.weixin.qq.com/cgi-bin/getimgdata?token=496425256&msgid=&mode=large&source=file&fileId=200457948&ow=-1]]></PicUrl>
+<Url><![CDATA[http://121/42/42/110/cloudy/app/poker]]></Url>
+</item>";
+
+        return $xmlStart . $itemTmp . $xmlEnd;
+
+    }
 
 }
 
