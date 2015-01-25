5cca174c2bdc2fb817d8bd7bc3680f47c9c348c4
cloud <792724991@qq.com>
2015-01-25 23:32:53 +0800

    the web page

---
+++ app/controller/web.php
@@ -0,0 +1,13 @@
+<?php
+
+$app->get('/', function() use($app){
+    $template_head = $app->view->fetch('view/template/common/head.html');
+    $template_header = $app->view->fetch('view/template/common/header.html');
+
+    $app->view->setData('head', $template_head);
+    $app->view->setData('header', $template_header);
+    $template_footer = $app->view->fetch('view/template/common/footer.html');
+    $app->view->setData('footer', $template_footer);
+    $app->view->display('view/template/web/index.html');
+
+});
---
+++ app/view/js/app/controller/web.js
@@ -0,0 +1,9 @@
+var eApp = angular.module('comic-app',[]);
+
+eApp.controller('webController',['$scope', '$http', webController]);
+function webController(){
+
+    console.log('漫界页面，施工中...');
+
+}
+
