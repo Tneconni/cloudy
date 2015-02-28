df9bc1293ef23200301a7342899af58c99709412
J <373800940@qq.com>
2015-02-28 11:33:05 +0800

    test the memcachee

--- app/settings.php
+++ app/settings.php
@@ -1,3 +1,12 @@
 <?php
 
-echo phpinfo();
+
+if(1){
+    $mem = new Memcache;
+    $mem->connect("127.0.0.1", 11211);
+    $mem->set('key1', 'This is first value', 0, 60);
+    $val = $mem->get('key1');
+
+}
+
+
