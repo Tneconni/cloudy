c9d61cfb06af5f23b11c325dbe1f91d8d9567249
J <373800940@qq.com>
2015-02-28 11:55:14 +0800

    test the memcache

--- app/settings.php
+++ app/settings.php
@@ -1,9 +1,9 @@
 <?php
 
 
-if(1){
+if( class_exists('Memcache') ){
     $mem = new Memcache;
-    $mem->connect("127.0.0.1", 11211);
+    $mem->connect("127.0.0.1", 11212);
     $mem->set('key1', 'This is first value', 0, 60);
     $val = $mem->get('key1');
     echo $val;
