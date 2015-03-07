2c384333092f6178ffad15f67cfff3144bfae323
cloud <792724991@qq.com>
2015-03-07 22:37:28 +0800

    updating the memcache flush

--- app/controller/memcache.php
+++ app/controller/memcache.php
@@ -1,10 +1,11 @@
 <?php
 
 require_once( BASEDIR . '/../library/pdo/my_pdo.php' );
-$app->get('/', function() use($app){
+$app->get('/flush', function() use($app){
     global $mem;
     if(is_a( $mem, 'Memcache' )){
         $mem->flush();
+        echo 'the memcache fas flushed';
     }
 });
 
