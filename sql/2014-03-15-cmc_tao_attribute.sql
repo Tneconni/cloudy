0dd3ecdf1583f7a25d0e61bfdc73d333dcba28f8
cloud <792724991@qq.com>
2015-03-15 13:34:41 +0800

    cmc_tao_attribute

---
+++ sql/2014-03-15-cmc_tao_attribute.sql
@@ -0,0 +1,10 @@
+-- 添加表属性 --
+CREATE TABLE `cmc_tao_attribute` (
+  `tao_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `price` DECIMAL(9,2) DEFAULT '0' NOT NULL,
+  `special` DECIMAL(9,2) DEFAULT '0' NOT NULL,
+  `discount` DECIMAL(5,2) DEFAULT '0' NOT NULL,
+  `like_count` INT(11) DEFAULT '0' NOT NULL,
+  `source` VARCHAR(16) DEFAULT 'tmall' NULL COMMENT 'tmall,tao',
+  PRIMARY KEY (`tao_id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
