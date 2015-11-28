3466c26870b918ffa370b8b0ec55012fe2d01cb2
cloud <792724991@qq.com>
2015-11-28 21:07:47 +0800

    the comic search module

--- app/controller/comic.php
+++ app/controller/comic.php
@@ -27,3 +27,31 @@ $app->get('/single/:comic_id', function( $comic_id ) use($app){
 
     $app->smarty->display('view/template/comic/detail.html');
 });
+
+$app->get('/search', function( ) use( $app ){
+
+
+    global $mem;
+    if(is_a( $mem, 'Memcache' )){
+        $template_head = $mem->get('template_head');
+        $template_header = $mem->get('template_header');
+        $template_footer = $mem->get('template_footer');
+    }else{
+        $template_head = $app->view->fetch('view/template/common/head.html');
+        $template_header = $app->view->fetch('view/template/common/header.html');
+        $template_footer = $app->view->fetch('view/template/common/footer.html');
+    }
+    $app->smarty->assign('head', $template_head);
+    $app->smarty->assign('header', $template_header);
+    $app->smarty->assign('footer', $template_footer);
+
+    $comic = new \Module\Comic();
+
+    $rank = $comic->rank(51, 0);
+//    $app->view->setData('rank', $rank);
+    $app->smarty->assign('rank', $rank); //print_r( $rank);
+
+    $app->smarty->display('view/template/comic/search.html');
+
+
+});
