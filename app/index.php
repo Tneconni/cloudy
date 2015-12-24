37823a12ab6d88e08b4357449a04eee2d539ce08
cloud <792724991@qq.com>
2015-12-24 21:58:26 +0800

    change uri

--- app/index.php
+++ app/index.php
@@ -21,7 +21,7 @@ if( strpos( $changeUri, '?' ) !== false ){
 	$changeUri = explode('?', $_SERVER['REQUEST_URI'])[0];
 
 }
-$changeUri = ($changeUri == '/' || $changeUri == '' ) ? '/app/gift/squad-list': $changeUri;
+$changeUri = ($changeUri == '/' || $changeUri == '' || $changeUri == '/app' || $changeUri == '/app/' ) ? '/app/gift/squad-list': $changeUri;
 
 $changeUriChange = explode('/app', $changeUri)[1];
 $changeUriChange = '/app' . $changeUriChange;
