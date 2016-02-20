19af8e66a3ba0a2b3277a90a20f41e820d86bd92
cloud <792724991@qq.com>
2016-02-20 15:17:27 +0800

    sql

---
+++ sql/2016-02-20-cmc_tao_attribut.sql
@@ -0,0 +1,3 @@
+ALTER  TABLE `cmc_tao_attribute` ADD `sellCount` INT(11) NOT NULL COMMENT '卖出数量';
+ALTER  TABLE `cmc_tao_attribute` ADD `reviewCount` INT(11) NOT NULL COMMENT '评价数量';
+ALTER  TABLE `cmc_tao_attribute` ADD `empointCount` INT(11) NOT NULL COMMENT '天猫积分数量';
